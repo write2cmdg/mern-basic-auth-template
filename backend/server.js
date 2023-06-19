5c2e023e676d63817867bfb9ec33a0074bb14d1e
Cristian Di Grandi <write2cmdg@gmail.com>
2023-06-19 15:53:23 -0400

    tablet testing

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,5 @@ const app = express()
 app.get('/', (req, res) => res.send('Server is ready'))
 
 app.listen(port, () => console.log(`server listeng on port ${port}`))
+
+// this is a message typed from my new tablet this is a message
