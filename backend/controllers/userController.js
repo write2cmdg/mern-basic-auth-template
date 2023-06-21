d0d0043a173bc8f5250f6abf7817792f9eb7cee7
Cristian Di Grandi <write2cmdg@gmail.com>
2023-06-21 01:02:33 -0400

    setup register bcrypt jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
-
+import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 //Desc: Auth user / set Token
 //Method: POST
@@ -18,8 +19,34 @@ const authUser = asyncHandler(async (req, res) => {
 //route: api/users
 //Access: public
 const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
 
-    res.status(200).json({message:'Register User'})
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+
 })
 
 
