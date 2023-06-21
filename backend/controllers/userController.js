d0b7d0bf4b52fa60debb808470add358b5e1191a
Cristian Di Grandi <write2cmdg@gmail.com>
2023-06-21 01:19:07 -0400

    added login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,10 +7,21 @@ import generateToken from '../utils/generateToken.js'
 //route: api/users/auth
 //Access: public
 const authUser = asyncHandler(async (req, res) => {
-    // res.status(401);
-    // throw new Error('Something went wrong') 
+const { email, password } = req.body
+const user = await User.findOne({ email })
 
-    res.status(200).json({message:'Auth User'})
+if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id)
+    res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email
+    })
+} else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+}
+
 })
 
 
