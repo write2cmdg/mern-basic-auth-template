baca486c758848ad89d18717a51cbab43ab1d99c
Cristian Di Grandi <write2cmdg@gmail.com>
2023-06-26 18:05:54 -0400

    added toastify

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector} from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import { useLoginMutation } from '../slices/usersApiSlice'
 import {setCredentials} from '../slices/authSlice'
+import { toast } from 'react-toastify';
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('')
@@ -31,7 +32,7 @@ const LoginScreen = () => {
             dispatch(setCredentials({ ...res }))
             navigate('/')
         } catch (err) {
-            console.log(err?.data?.message || err.error)
+            toast.error(err?.data?.message || err.error)
         }
     }
   return (
