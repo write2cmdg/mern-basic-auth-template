118efb6f5e68db48659bdceea9d42e2263b4487b
Cristian Di Grandi <write2cmdg@gmail.com>
2023-06-27 01:05:40 -0400

    added Logout functionality to client side

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,10 +2,32 @@ import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap'
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
+import { useLogoutMutation } from '../slices/usersApiSlice'
+import { clearCredentials } from '../slices/authSlice'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 
 const Header = () => {
     const { userInfo } = useSelector((state) => state.auth)
+
+    const dispatch = useDispatch()
+
+    const navigate = useNavigate()
+
+    const [logoutApiCall] = useLogoutMutation()
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap()
+            dispatch(clearCredentials())
+            navigate('/')
+        } catch (err) {
+           toast.error(err) 
+        }
+    }
+
+
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='sm' collapseOnSelect>
@@ -24,7 +46,7 @@ const Header = () => {
                                                 Profile
                                             </NavDropdown.Item>
                                         </LinkContainer>
-                                        <NavDropdown.Item>
+                                        <NavDropdown.Item onClick={ logoutHandler }>
                                             Logout
                                         </NavDropdown.Item>
                                     </NavDropdown> 
